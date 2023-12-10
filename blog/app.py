639050a9e06ce1d61eb9cfeb90ae5127e160eaff
Denys Morozov <ajax3101@gmail.com>
2023-12-10 21:52:54 +0200

    add structure

--- blog/app.py
+++ blog/app.py
@@ -6,8 +6,34 @@ app =Flask(__name__)
 
 
 @app.route("/")
-def front():
+def frontpage():
     return "ok"
-    
 
+@app.route("/about")
+def aboutpage():
+    return "ok"
+
+@app.route("/contact")
+def contactpage():
+    return "ok"
+
+@app.route("/post/<post_id:int>", endpoint="post")
+def postpage(post_id):
+    pass
+
+@app.route("/post/<post_id:int>/edit", method=["GET", "POST"], endpoint="edit")
+def postedit(post_id):
+    pass
+
+@app.route("/post/new", method=["GET", "POST"], endpoint="new")
+def postnew():
+    pass
+
+@app.route("/login")
+def loginpage():
+    pass
+
+@app.route("/logout")
+def logoutpage():
+    pass
 
